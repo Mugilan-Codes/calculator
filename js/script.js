3c0e25618084b56eebf03b21c5d5bc467e984c57
Mugilan <mugilancodes@gmail.com>
2020-06-23 18:30:16 +0530

    Revamped Styles

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ const mul = (a, b) => a * b;
 const div = (a, b) => a / b;
 
 const operate = (operator, a, b) => operator(a, b);
+
+const numberButtons__div = document.querySelectorAll('#buttons .number');
+
+const displayResult = document.getElementById('display');
+
+let displayValue = '';
+
+const fillDisplay = function () {
+  displayValue += this.value;
+
+  displayResult.textContent = displayValue;
+};
+
+numberButtons__div.forEach((number) =>
+  number.addEventListener('click', fillDisplay)
+);
