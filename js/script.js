45a80a004386306682d5ac5df6ec6be190d8802f
Mugilan <mugilancodes@gmail.com>
2020-06-23 20:30:17 +0530

    Number Inputs

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@ const mul = (a, b) => a * b;
 const div = (a, b) => a / b;
 
 const operate = (operator, a, b) => operator(a, b);
+
+const numbers__button = document.querySelectorAll('#buttons .number');
+const resultDisplay__div = document.getElementById('result');
+
+let currValue = '',
+  prevValue = '',
+  operator;
+const fillDisplay = function () {
+  // prevents two or more decimal points
+  if (currValue.includes('.') && this.value === '.') return;
+
+  currValue += this.value;
+
+  resultDisplay__div.textContent = currValue;
+};
+
+numbers__button.forEach((number) =>
+  number.addEventListener('click', fillDisplay)
+);
