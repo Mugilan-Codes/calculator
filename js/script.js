fb5620a8849360520b82ed4d33efbf333062a2e3
Mugilan <mugilancodes@gmail.com>
2020-06-26 19:30:10 +0530

    Needs more Work

--- js/script.js
+++ js/script.js
@@ -22,6 +22,7 @@ const numbers__button = document.querySelectorAll('#buttons .number');
 const historyDisplay__div = document.getElementById('history');
 const resultDisplay__div = document.getElementById('result');
 const operators__button = document.querySelectorAll('#buttons .operator');
+const operations = document.querySelectorAll('#buttons .operation');
 
 let storedValue = '',
   prevValue = '',
@@ -64,4 +65,33 @@ const calculateAnswer = function () {
 
 operators__button.forEach((operator) =>
   operator.addEventListener('click', calculateAnswer)
+);
+
+const performOperation = function () {
+  console.log(this.id);
+  if (this.id === 'negator') {
+    currValue *= -1;
+    resultDisplay__div.textContent = currValue;
+  }
+  if (this.id === 'all-clear') {
+    resultDisplay__div.textContent = '0';
+    historyDisplay__div.textContent = '';
+    currValue = '';
+    operator = '';
+    prevValue = '';
+  }
+  if (this.id === 'equal-to') {
+    currValue = operate(operator, prevValue, currValue);
+    historyDisplay__div.textContent = historyDisplay__div.textContent = `${prevValue} ${operatorSymbol} ${currValue}`;
+    resultDisplay__div.textContent = currValue;
+    currValue = '';
+  }
+  if (this.id === 'backspace') {
+    currValue = currValue.slice(0, -1);
+    resultDisplay__div.textContent = currValue;
+  }
+};
+
+operations.forEach((operation) =>
+  operation.addEventListener('click', performOperation)
 );
